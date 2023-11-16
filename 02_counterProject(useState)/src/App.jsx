4513d3a7e76ba16c12273714c3d4b1c4708aa179
Developersunil001 <sunilkumar088839@gmail.com>
2023-11-16 23:24:04 +0530

    counter Project using useState in React

---
+++ 02_counterProject(useState)/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [count, setCount] = useState(0)
+
+  let addCount = () => {
+    console.log("Added!")
+    if (count < 10) {
+      setCount((addCount)=>addCount + 1);
+    }
+  }
+
+  let removeCount = () => {
+    console.log("removed!")
+    if (count > 0) {
+      setCount((removeCount)=> removeCount - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>counter project {count}</h1>
+      <button onClick={addCount}>Add</button>         
+      <button onClick={removeCount}>Remove</button>
+    </>
+  )
+}
+
+export default App
