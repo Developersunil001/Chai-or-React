c7993c49fcad148f27391f757026b4dda5deb4b9
Developersunil001 <sunilkumar088839@gmail.com>
2024-01-03 16:43:17 +0530

    done with Reduxtoolkit

---
+++ 12_reduxToolKit/src/App.jsx
@@ -0,0 +1,17 @@
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* <h1 className='bg-red-500' >Make Your Todo's</h1> */}
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
+
+
